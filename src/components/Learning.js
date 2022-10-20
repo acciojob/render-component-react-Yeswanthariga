a6596a9cb7194ec3a1ff1ea170a55fe0a7b75e96
Ariga Yeswanth Kumar <102847173+Yeswanthariga@users.noreply.github.com>
2022-10-20 12:54:54 +0530

    Create Learning.js

---
+++ src/components/Learning.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Learning extends Component {
+  render() {
+    return (
+      <div>
+        <p>I am learning React. My life is getting better.</p>
+      </div>
+    );
+  }
+}
+
+export default Learning;
